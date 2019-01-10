f70231fef85c1c3a947a02e038d26992ea344545
jrbeverly <jrbeverly@live.com>
2019-01-09 21:25:38 -0500

    Setup the artifacts bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,40 @@
-variable "name" {
-  default = "Example"
+locals {
+  tags = {
+    TieredStorage = "enabled"
+    AutoExpire    = "true"
+    ExpiresIn     = "${var.expiration_days}"
+  }
+}
+
+resource "aws_s3_bucket" "artifacts" {
+  bucket        = "${var.bucket}"
+  acl           = "private"
+  force_destroy = false
+
+  tags = "${merge(local.tags, var.tags)}"
+
+  lifecycle_rule {
+    id      = "tiered"
+    enabled = true
+
+    tags {
+      Rule        = "tiered"
+      AutoClean   = "true"
+      Description = "Transitions the objects between storage classes, until eventual expiration."
+    }
+
+    transition {
+      days          = "${var.standard_transition_days}"
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = "${var.glacier_transition_days}"
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = "${var.expiration_days}"
+    }
+  }
 }

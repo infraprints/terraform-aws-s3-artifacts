d04699779c2051b3b4bfdf27ff5795c95934a52a
jrbeverly <jrbeverly@live.com>
2019-04-20 12:59:25 -0400

    Add a sample

---
+++ samples/main.tf
@@ -0,0 +1,13 @@
+module "build_artifacts" {
+  source = "git::https://gitlab.com/infraprints/modules/aws/s3-artifacts"
+
+  bucket                   = "infraprints-s3-artifacts"
+  standard_transition_days = 10
+  glacier_transition_days  = 30
+  expiration_days          = 365
+
+  tags = {
+    Longevity  = "Yearly"
+    Expiration = "True"
+  }
+}

6c90888b37e413a12dcfb892cdacbda465f3fde6
jrbeverly <jrbeverly@live.com>
2019-06-02 13:14:47 -0400

    Uprgade to terraform 0.12

--- main.tf
+++ main.tf
@@ -2,39 +2,40 @@ locals {
   tags = {
     TieredStorage = "enabled"
     AutoExpire    = "true"
-    ExpiresIn     = "${var.expiration_days}"
+    ExpiresIn     = var.expiration_days
   }
 }
 
 resource "aws_s3_bucket" "artifacts" {
-  bucket        = "${var.bucket}"
+  bucket        = var.bucket
   acl           = "private"
   force_destroy = false
 
-  tags = "${merge(local.tags, var.tags)}"
+  tags = merge(local.tags, var.tags)
 
   lifecycle_rule {
     id      = "tiered"
     enabled = true
 
-    tags {
+    tags = {
       Rule        = "tiered"
       AutoClean   = "true"
       Description = "Transitions the objects between storage classes, until eventual expiration."
     }
 
     transition {
-      days          = "${var.standard_transition_days}"
+      days          = var.standard_transition_days
       storage_class = "STANDARD_IA"
     }
 
     transition {
-      days          = "${var.glacier_transition_days}"
+      days          = var.glacier_transition_days
       storage_class = "GLACIER"
     }
 
     expiration {
-      days = "${var.expiration_days}"
+      days = var.expiration_days
     }
   }
 }
+
